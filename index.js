5c5194dc4cbbe4aa590a5e149513ba58036062dc
Yeassin7376 <yeassinali17@gmail.com>
2025-09-21 13:44:20 +0600

    added a new cros origin a live link of the client side that deployed on vercel

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://newspaper-b11a12-auth.web.app']
+    origin: ['http://localhost:5173', 'https://newspaper-b11a12-auth.web.app', 'https://newspaper-client-ten.vercel.app/', 'https://newspaper-client.vercel.app', '*']
 }));
 app.use(express.json());
 
